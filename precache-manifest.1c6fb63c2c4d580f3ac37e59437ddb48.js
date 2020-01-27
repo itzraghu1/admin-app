a44df22984a5d451bf2645f1fe33f5cec18e0b62
logimetrix <logimetrix@gmail.com>
2020-01-27 11:03:44 +0530

    Updates

---
+++ precache-manifest.1c6fb63c2c4d580f3ac37e59437ddb48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b74c43061e9ed1a68f294c0c59e7e53a",
+    "url": "/admin-app/index.html"
+  },
+  {
+    "revision": "5319bfa5c93e57e1a8e5",
+    "url": "/admin-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "366e59515dc8c75d6c7e",
+    "url": "/admin-app/static/js/2.e29e2b24.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/admin-app/static/js/2.e29e2b24.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5319bfa5c93e57e1a8e5",
+    "url": "/admin-app/static/js/main.ac8c8d14.chunk.js"
+  },
+  {
+    "revision": "c1a9ce18bbe45d307693",
+    "url": "/admin-app/static/js/runtime-main.a4107531.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/admin-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
